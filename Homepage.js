0e213dd51ce89b2a2b21eae56ba4911656aee1d9
rivkagribov <rivka.gribov@gmail.com>
2025-03-31 21:03:22 -0400

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -1,1 +1,11 @@
+async function main() {  
+  settingsButton.addEventListener('click', function() {
+        alert('How To Play:\n1. Guess the Wordle in 5 tries.\n2. Each guess must be a valid 5-letter word.\n3. Tell Rivka "I love you" and give her a kiss. 💖');
+    });
 
+    heartButton.addEventListener('click', function() {
+        alert('More');
+    });
+}
+
+main();
